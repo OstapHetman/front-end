51161a9212e4208e6b72a7919ca48b2241bd1b0d
OstapHetman <prickly4528@gmail.com>
2017-05-10 22:28:09 +0300

    group components by modules and add more descriptive

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,31 +1,30 @@
+// MODULE
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 import { HttpModule, Http } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-// import { TranslateModule, MissingTranslationHandler } from 'ng2-translate';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { MaterialModule } from '@angular/material';
-// import {TranslateModule} from '@ngx-translate/core';
-// import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { MyDatePickerModule } from 'mydatepicker';
+// ROUTE
 import { routes } from './app.router';
+// COMPONENT
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { ConfigurationComponent } from './configuration/configuration.component';
-import { AdministrationComponent } from './administration/administration.component';
-// import { MyMissingTranslationHandler } from './missingtemplate.component';
 import { UsernameComponent } from './username/username.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
+import { LanguageComponent } from './language/language.component';
 import { LangboxComponent } from './langbox/langbox.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { AdministrationComponent } from './administration/administration.component';
 import { ContainerComponent } from './container/container.component';
-import { TimeSettingComponent } from './time-setting/time-setting.component';
 import { EmailFormComponent } from './email-form/email-form.component';
 import { LdapConfigurationComponent } from './ldap-configuration/ldap-configuration.component';
-import { LanguageComponent } from './language/language.component';
+import { TimeSettingComponent } from './time-setting/time-setting.component';
 
 @NgModule({
   declarations: [
